544aaa2322b93b27d1a1be941e904435cda689a9
Nat Cannon <natcannon13@gmail.com>
2026-07-09 16:58:30 -0700

    Vote Button Functionality
    Replaced vote command with voting buttons.

--- buttons/vote.js
+++ buttons/vote.js
@@ -1,6 +1,5 @@
 const config_util = require("../utils/config_util.js");
 const GameManager = require("../game/GameManager.js");
-const roles_util = require("../utils/roles_util.js");
 const WerewordsGame = require("../game/WerewordsGame.js");
 const Player = require("../game/Player.js");
 
@@ -10,5 +9,34 @@ async function run(interaction, guildId, user, option){
     if(!game){
         return;
     }
+
+    let voter = interaction.member;
+
+    if(!game.players.has(voter.id)){
+        return interaction.reply({content: "You are not playing!", ephemeral: true});
+    }
     
+    if(voter.id === user){
+        return interaction.reply({content: "You cannot vote for yourself!", ephemeral: true});
+    }
+
+    if(game.phase === "seerKill"){
+        if(voter.displayName !== game.werewolfSpokesman){
+            return interaction.reply({content: "You are not the Voting Werewolf!", ephemeral: true});
+        }
+        else{
+            await game.seerVoteReceived(user);
+        }
+    }
+    else if(game.phase === "werewolfVote"){
+        game.players.get(voter.id).vote = user;
+    }
+    else{
+        return interaction.reply({content: "You cannot use this right now!", ephemeral: true});
+    }
+    await interaction.deferUpdate();
+    return interaction.followUp({content: "Vote cast successfully!", ephemeral: true});
+}
+module.exports = {
+    run
 }
